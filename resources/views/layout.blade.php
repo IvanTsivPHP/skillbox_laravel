293b7d47b1e9b323bb05fde55c267c7fe6c46ed1
IvanTsivPHP <ivan.tsivanuk@gmail.com>
2021-11-02 13:02:34 +0000

    v.1

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -62,7 +62,29 @@
                 <a class="link-secondary" href="#" aria-label="Search">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
                 </a>
-                <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+                @guest
+                    @if (Route::has('login'))
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    @endif
+
+                    @if (Route::has('register'))
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    @endif
+                @else
+                    <a>{{ Auth::user()->name }}:</a>
+                    <div>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                    </li>
+                @endguest
             </div>
         </div>
         @include('main')
@@ -71,7 +93,9 @@
         <div class="col-md-8">
     @yield('content')
         </div>
-    @include('layouts.sidebar')
+        @section('sidebar')
+            @include('layouts.sidebar')
+        @show
     </div>
 </main>
 </body>
