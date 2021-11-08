d2b3526c0696f4c60b718e378ccd41e758f38a15
IvanTsivPHP <ivan.tsivanuk@gmail.com>
2021-11-08 09:59:39 +0000

    v.1

---
+++ app/Notifications/ArticleEdited.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Article;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ArticleEdited extends Notification
+{
+    use Queueable;
+
+    private $article;
+    /**
+     * Create a new notification instance.
+     *
+     * @param Article $article
+     */
+    public function __construct(Article $article)
+    {
+        $this->article = $article;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Статья ' . $this->article->name . ' была отредактирована.')
+                    ->action('Ссылка на статью: ', url('/articles/' . $this->article->id));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
